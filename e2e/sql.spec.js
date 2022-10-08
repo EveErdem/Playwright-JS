0953f5e90580cef40a24a63896cf11d13e9645e5
EceNarin <la.crymosa321@gmail.com>
2022-10-08 17:07:53 +0300

    playwright

--- e2e/sql.spec.js
+++ e2e/sql.spec.js
@@ -11,4 +11,6 @@ Brute Force Attack simulation	"1) Go to http://demo.guru99.com/Security/SEC_V1/i
 2) Enter Userid
 3) Enter Password
 4) Click Login
-5) Repeat steps 1-4 until you find the correct password"	Password = wordlist from dictionary, common names, dates etc	IP banning after 3 wrong password attempts*/
+5) Repeat steps 1-4 until you find the correct password"	Password = wordlist from dictionary, common names, dates etc	IP banning after 3 wrong password attempts
+ecenarin1990@gmail.com
+*/
